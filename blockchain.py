3f1d8e7f7eb11fa6e12b269e28a94ecdff2e4285
jethronap <efthioa@gmail.com>
2019-06-13 21:17:29 +0300

    added ProofOfWork algorithm & validated result

--- blockchain.py
+++ blockchain.py
@@ -64,3 +64,32 @@ class Blockchain(object):
     def last_block(self):
         # returns the last block in the chain
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        Proof of Work Algorithm:
+        - Find a number x such that hash(xx') contains 4 leading zeroes, where x is the previous x'
+        - x is the previous proof and x' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, else False
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
