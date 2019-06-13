ef3728b00723270d6ddb75fe2868f1cece97e7a8
jethronap <efthioa@gmail.com>
2019-06-13 22:23:48 +0300

    added new node to the list of nodes

--- blockchain.py
+++ blockchain.py
@@ -15,6 +15,21 @@ class Blockchain(object):
         # create the first block
         self.new_block(previous_hash=1, proof=100)
 
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
+        """
+
+        parsed_url = urlparse(address)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            # Accepts an URL without scheme like '192.168.0.5:5000'.
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
+
     def new_block(self, proof, previous_hash=None):
         """
         creates a new block and adds it to the chain
