3f63d523b00ba6f5f5136e58ceed61a35b130ce2
jethronap <efthioa@gmail.com>
2019-06-13 21:29:49 +0300

    APIied blockchain with flask routing

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
 from time import time
+from textwrap import dedent
+from uuid import uuid4
+
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -93,3 +97,31 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    # Instantiate Node
+    app = Flask(__name__)
+
+    # Generate a globally unique address for this Node
+    node_identifier = str(uuid4()).replace('-', '')
+
+    # Instantiate the Blockchain
+    blockchain = Blockchain()
+
+    @app.route('/mine', methods=['GET'])
+    def mine():
+        return "We'll mine a new block"
+
+    @app.route('/transactions/new', methods=['POST'])
+    def new_transaction():
+        return "We'll add a new transaction"
+
+    @app.route('/chain', methods=['GET'])
+    def full_chain():
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain),
+        }
+        return jsonify(response), 200
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
