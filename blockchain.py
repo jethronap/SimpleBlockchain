507102e1ef2910e37a2f16d0ddd02b4d6d0d2779
jethronap <efthioa@gmail.com>
2019-06-13 22:14:45 +0300

    fixed imports

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,10 @@
 import hashlib
 import json
 from time import time
-from textwrap import dedent
+#from textwrap import dedent
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
